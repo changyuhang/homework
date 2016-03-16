c0b03edede211fa0dc020f9b5787b657ee28ada6
張語航 <00257122@ntou.edu.tw>
2016-03-16 16:40:39 +0800

    專題作業3
    完成

--- 專題作業/3/hw3.cpp
+++ 專題作業/3/hw3.cpp
@@ -1,39 +1,39 @@
-#define _USE_MATH_DEFINES
-
 #include "opencv2/opencv.hpp"
 #include<iostream>
 #include<cmath>
-
-
 #define h imginital->height
 #define w imginital->width
+#define nh cvRound(fabs(h*vcos)+fabs(w*vsin))
+#define nw cvRound(fabs(w*vcos)+fabs(h*vsin))
 void main() {
 	IplImage *imginital = cvLoadImage("a.jpg", 1);
 	IplImage *imgfinal;
-	IplImage *imgtemp;
-	int max_hw;
-	max_hw = (int)(sqrt(imginital->height*imginital->height + imginital->width*imginital->width) + 1);
-	float dis_x, dis_y;
-	dis_x = (max_hw - w) / 2;
-	dis_y = (max_hw - h) / 2;
-
-	int turn;
-	std::cout << "請輸入1(90度),2(180度),3(270度):";
+	double turn;
+	std::cout << "請輸入旋轉幾度:";
 	std::cin >> turn;
-	imgfinal = cvCreateImage(CvSize(max_hw, max_hw), IPL_DEPTH_8U, 3);
-
-	turn = turn*(M_PI / 180);
+	turn = turn*(CV_PI / 180);
 	double vcos = cos(turn), vsin = sin(turn);
-
-	for (int i = 0; i < imginital->height; i++) {
-		for (int j = 0; j < imginital->width; j++) {
-			int nx = (int)(vcos*(j+dis_x) - vsin*(i+dis_y));
-			int ny = (int)(vsin*(j+dis_x) + vcos*(i+dis_y));
-			int imginital_imgData = imginital->widthStep * i + j * 3;
-			int imgfinal_imgData = imgfinal->widthStep *ny + nx * 3;//+ (int)((imgfinal->widthStep * dis_y) + (dis_x * 3)); //上面空行+ 每一行的空格
-			imgfinal->imageData[imgfinal_imgData] = imginital->imageData[imginital_imgData];
-			imgfinal->imageData[imgfinal_imgData + 1] = imginital->imageData[imginital_imgData + 1];
-			imgfinal->imageData[imgfinal_imgData + 2] = imginital->imageData[imginital_imgData + 2];
+	imgfinal = cvCreateImage(CvSize(nw, nh), IPL_DEPTH_8U, 3);
+	double offset_x, offset_y;
+	offset_x = -nw / 2.0f * vcos - nh / 2.0f * vsin + w / 2.0f;
+	offset_y = nw / 2.0f * vsin - nh / 2.0f * vcos + h / 2.0f;
+	std::cout << offset_x << " " << offset_y << std::endl;
+	//offset_x = 0;
+	//offset_y = 0;
+	for (int ny = 0; ny < imgfinal->height; ny++) {
+		for (int nx = 0; nx < imgfinal->width; nx++) {
+			int x = (int)(nx*vcos + ny*vsin + 0.5) + offset_x;
+			int y = (int)(-nx*vsin + ny*vcos + 0.5) + offset_y;
+			int imginital_imgData = imginital->widthStep * y + x * 3;
+			int imgfinal_imgData = imgfinal->widthStep *ny + nx * 3;
+			if (x<0 || y<0 || x >= imginital->width || y >= imginital->height){
+				imgfinal->imageData[imgfinal_imgData] = imgfinal->imageData[imgfinal_imgData + 1] = imgfinal->imageData[imgfinal_imgData + 2] = 127;
+			}
+			else{
+				imgfinal->imageData[imgfinal_imgData] = imginital->imageData[imginital_imgData];
+				imgfinal->imageData[imgfinal_imgData + 1] = imginital->imageData[imginital_imgData + 1];
+				imgfinal->imageData[imgfinal_imgData + 2] = imginital->imageData[imginital_imgData + 2];
+			}
 		}
 	}
 
@@ -41,35 +41,3 @@ void main() {
 	cvShowImage("'Result", imgfinal);
 	cvWaitKey(0);
 }
-
-//#define _USE_MATH_DEFINES
-//
-//#include "opencv2/opencv.hpp"
-//#include<iostream>
-//#include<cmath>
-//
-//#define h imginital->height
-//#define w imginital->width
-//void main() {
-//	IplImage *imginital = cvLoadImage("a.jpg", 1);
-//	IplImage *imgfinal;
-//	int turn;
-//	std::cout << "請輸入1(90度),2(180度),3(270度):";
-//	std::cin >> turn;
-//	if (turn == 1 || turn == 3) imgfinal = cvCreateImage(CvSize(h, w), IPL_DEPTH_8U, 3);
-//	else imgfinal = cvCreateImage(CvSize(w, h), IPL_DEPTH_8U, 3);
-//	for (int i = 0; i < h; i++) {
-//		for (int j = 0; j < w; j++) {
-//			//int imginital_imgData = imginital->widthStep * (turn != 3 ? (h - i - 1) : i) + (turn == 2 ? (w - j) : j) * 3;
-//			//int imgfinal_imgData = (turn == 2 ? (imgfinal->widthStep * i + j * 3) : (imgfinal->widthStep * j + i * 3));
-//			int imginital_imgData = imginital->widthStep * i + j * 3;
-//			int imgfinal_imgData = abs(imginital->widthStep * (i*cos(M_PI / 180 * turn) - j*sin(M_PI / 180 * turn)) + (i*sin(M_PI / 180 * turn) + j*cos(M_PI / 180 * turn)) * 3);
-//			imgfinal->imageData[imgfinal_imgData] = imginital->imageData[imginital_imgData];
-//			imgfinal->imageData[imgfinal_imgData + 1] = imginital->imageData[imginital_imgData + 1];
-//			imgfinal->imageData[imgfinal_imgData + 2] = imginital->imageData[imginital_imgData + 2];
-//		}
-//	}
-//	cvShowImage("Original", imginital);
-//	cvShowImage("'Result", imgfinal);
-//	cvWaitKey(0);
-//}
